50dd3f1a832d176f1db5823d6a2b2eea4d26c7a6
geronimo <luciano.geronimo.fnord@gmail.com>
2016-10-03 21:56:06 -0300

    Agora a 1a parte do nehe4 está funcionando. Só falta por a carga a partir de um .obj funcionando na 2a parte.

--- nehe_04/nehe04.cpp
+++ nehe_04/nehe04.cpp
@@ -1,14 +1,16 @@
-#include <GL\glew.h>
-#include <GL\GL.h>
-#include <GL\glut.h>
+#include <glew.h>
+#include <gl.h>
+#include <glut.h>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include "shader.h"
 
-#include <glm.hpp>
-#include <gtc\matrix_transform.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <cstring>
+
 using namespace std;
 using namespace glm;
 const GLfloat fov = 45.0f;
@@ -28,14 +30,20 @@ struct Triangulo
 Triangulo tri;
 
 MyShader myshader;
-
+#ifdef WIN32
+const string vsPath = "C://programacao//comp_grafica//src//nehe_03//vertexShader.vertexshader";
+const string fsPath = "C://programacao//comp_grafica//src//nehe_03//fragmentShader.fragmentshader";
+#else
+const string vsPath = "//home//geronimo//programacao//estudos_cg//nehe_03//vertexShader.vertexshader";
+const string fsPath = "//home//geronimo//programacao//estudos_cg//nehe_03//fragmentShader.fragmentshader";
+#endif
 void initResources()
 {	
 	glEnable(GL_DEPTH_TEST);
 	//Shader
-	myshader = CreateShaderProgram("C://programacao//comp_grafica//src//nehe_04//vertexShader.vertexshader",
-								   "C://programacao//comp_grafica//src//nehe_04//fragmentShader.fragmentshader");
-	//Gambi pra inicializar o buffer
+	myshader = CreateShaderProgram(vsPath, fsPath);
+
+        //Gambi pra inicializar o buffer
 	static const GLfloat localData[] = {
 		-1.0f, -1.0f, 0.0f,
 		1.0f, -1.0f, 0.0f,
@@ -86,7 +94,7 @@ void keyboard(unsigned char key, int x, int y)
 
 void reshape(int w, int h)
 {
-	//A matrix de projeção depende da largura e altura da tela.
+	//A matrix de projeï¿½ï¿½o depende da largura e altura da tela.
 	screenWidth = w;
 	screenHeight = h;
 }
@@ -101,10 +109,10 @@ void display(void)
 	glm::mat4 projection = glm::perspective<GLfloat>(fov, screenWidth / screenHeight, 0.1f, 100.f);
 	glm::mat4 view = glm::lookAt(glm::vec3(1, 1, -4), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	glm::mat4 model = glm::mat4(1.0f);
-	glm::mat4 mvp = projection * view * model; // A multiplicação é na ordem inversa do nome.
+	glm::mat4 mvp = projection * view * model; // A multiplicaï¿½ï¿½o ï¿½ na ordem inversa do nome.
 	//agora passa pro shader
 	glUniformMatrix4fv(myshader.uniforms.at("mvp"), 1, GL_FALSE, &mvp[0][0]);
-	//Eu sei que o nome do atributo dos vértices é vertexPosition_modelspace.
+	//Eu sei que o nome do atributo dos vï¿½rtices ï¿½ vertexPosition_modelspace.
 	//Usando o atributo da geometria.
 	glEnableVertexAttribArray(myshader.attributes["vertexPosition_modelspace"]);
 	glBindBuffer(GL_ARRAY_BUFFER, tri.vertexbuffer);
@@ -116,7 +124,7 @@ void display(void)
 		0,                  // stride
 		(void*)0            // array buffer offset
 	);
-	//Agora é o atributo da cor
+	//Agora ï¿½ o atributo da cor
 	glEnableVertexAttribArray(myshader.attributes["vertexColor"]);
 	glBindBuffer(GL_ARRAY_BUFFER, tri.vertexcolor);
 	glVertexAttribPointer(
@@ -127,7 +135,7 @@ void display(void)
 		0,                  // stride
 		(void*)0            // array buffer offset
 	);
-	//Agora é a array
+	//Agora ï¿½ a array
 	/* Push each element in buffer_vertices to the vertex shader */
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, tri.elementBuffer);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, (void*)0);
@@ -139,7 +147,7 @@ void display(void)
 	glutSwapBuffers();
 }
 
-//Função principal do programa.
+//Funï¿½ï¿½o principal do programa.
 int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
@@ -153,7 +161,7 @@ int main(int argc, char *argv[])
 	{
 		const GLubyte* _err = glewGetErrorString(glError);
 		std::string str((char*)_err);
-		std::cerr << "Erro na inicialização da GLEW:" << str << std::endl;
+		std::cerr << "Erro na inicializaï¿½ï¿½o da GLEW:" << str << std::endl;
 		return EXIT_FAILURE;
 	}
 	initResources();
